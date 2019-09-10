50c05b76b12045f0d375bd546dfdbf43d4cdbeef
Teslothorcha <juandavidmarinbernal@gmail.com>
2019-09-10 17:49:26 +0000

    [Feature]  search for last digit and act upon it to print message

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 import random
+sh = "Last digit of "
 number = random.randint(-10000, 10000)
 if number < 0:
-    lnumber = (number * -1)%10
+    l = (number * -1) % 10
 else:
-    lnumber = number%10
-if lnumber > 5:
-    print('Last digit of {:d} is {:d} and is greater than {:d}'.format(number,lnumber,5))
-elif lnumber == 0:
-    print('Last digit of {:d} is {:d} and is {:d}'.format(number,lnumber,0))
-elif lnumber < 6 and lnumber != 0:
-    print('Last digit of {:d} is {:d} and is less than {:d} and not {:d}'.format(number,lnumber,5,0))
+    l = number % 10
+if l > 5:
+    print('{:s}{:d} is {:d} and is greater than 5'.format(sh, number, l))
+elif l == 0:
+    print('{:s}{:d} is {:d} and is 0'.format(sh, number, l))
+elif l < 6 and l != 0:
+    print('{:s}{:d} is {:d} and is less than 6 and not 0'.format(sh, number, l))
