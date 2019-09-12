944e918703969603228ce6ca60506f94011ce26b
Teslothorcha <juandavidmarinbernal@gmail.com>
2019-09-12 23:00:27 +0000

    [Fix] Print arguments of input

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
-import sys
-count = 0
-f = 0
-if len(sys.argv) == 2:
-    print("{:d} argument:".format(len(sys.argv) - 1))
-elif len(sys.argv) == 1:
-    print("{:d} arguments.".format(len(sys.argv) - 1))
-else:
-    print("{:d} arguments:".format(len(sys.argv) - 1))
-for elemen in sys.argv:
-    if count != 0:
-        print("{:d}: {:s}".format(count, elemen))
-        count += 1
-        f = 1
-    if f == 0:
-        count += 1
+if __name__ == "__main__":
+   import sys
+   count = 0
+   f = 0
+   if len(sys.argv) == 2:
+      print("{:d} argument:".format(len(sys.argv) - 1))
+   elif len(sys.argv) == 1:
+      print("{:d} arguments.".format(len(sys.argv) - 1))
+   else:
+      print("{:d} arguments:".format(len(sys.argv) - 1))
+      for elemen in sys.argv:
+         if count != 0:
+            print("{:d}: {:s}".format(count, elemen))
+            count += 1
+            f = 1
+         if f == 0:
+               count += 1
