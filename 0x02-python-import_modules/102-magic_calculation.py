a2bd16ae348fa431595039ac7a566ef12540e841
Teslothorcha <juandavidmarinbernal@gmail.com>
2019-09-13 04:25:28 +0000

    [Feature] Cracking bytecode

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -5,6 +5,9 @@ def magic_calculation(a, b):
         c = add(a, b)
         for i in range(4, 6):
             c = add(c, i)
-            return c
+        return c
     else:
         return sub(a, b)
+
+from dis import dis
+dis(magic_calculation)
