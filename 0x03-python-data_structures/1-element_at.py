1cc0659e2eed86f57212da3672abb40793ccb121
Teslothorcha <juandavidmarinbernal@gmail.com>
2019-09-17 18:11:25 +0000

    [Fix] implement a loop

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,4 +2,6 @@
 def element_at(my_list, idx):
     if (idx < 0 or idx > len(my_list)):
         return
-    return my_list[idx:idx + 1]
+    for count in range(len(my_list)):
+        if (count == idx):
+            return my_list[idx:idx + 1]
