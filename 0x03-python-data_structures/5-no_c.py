d2adbe289c2a6506eb4cfa8c04bda382bf00c610
Teslothorcha <juandavidmarinbernal@gmail.com>
2019-09-18 00:13:30 +0000

    [Fix] replace forbidden function

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def no_c(my_string):
-   my_string = my_string.replace('C', '')
-   my_string = my_string.replace('c', '')
-   return my_string
+    my_string = my_string.translate({ord(i): None for i in 'Cc'})
+    return my_string
