db373f550e914499ac4f718568c6a6cced0662ee
Teslothorcha <juandavidmarinbernal@gmail.com>
2019-09-18 02:50:35 +0000

    [Feature] cdeletes  item from list at given index

---
+++ 0x03-python-data_structures/11-delete_at.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def delete_at(my_list=[], idx=0):
+    if idx == 0 or idx > len(my_list):
+        return
+    else:
+        for count in my_list:
+            if count == idx:
+                del my_list[count]
+    return(my_list)
