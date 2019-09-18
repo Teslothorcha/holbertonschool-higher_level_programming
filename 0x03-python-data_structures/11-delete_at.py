9ca6cdf89a0527f97dff08bbac6b4c8ce16dbbae
Teslothorcha <juandavidmarinbernal@gmail.com>
2019-09-18 02:59:23 +0000

    [Fix] delete innecesary loop

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -3,7 +3,5 @@ def delete_at(my_list=[], idx=0):
     if idx < 0 or idx >= len(my_list):
         return(my_list)
     else:
-        for count in my_list:
-            if count == idx:
-                del my_list[count]
+        del my_list[count]
     return(my_list)
