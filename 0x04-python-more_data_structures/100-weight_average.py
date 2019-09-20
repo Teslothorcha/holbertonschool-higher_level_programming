792bf3940d4ee70823d09019ceef44e64cd09a77
teslothorcha <juandavidmarinbernal@gmail.com>
2019-09-20 03:18:04 +0000

    [Feature] calculate weight avergae

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    final = 0
+    wei = 0
+    for count in my_list:
+        final += count[0] * count[1]
+        wei += count[1]
+    return final/wei
