11516cd8b4168b4474adc89c2d213d39977d385c
teslothorcha <juandavidmarinbernal@gmail.com>
2019-09-20 02:42:59 +0000

    [Fix] add case when input is not str

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     romans = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    if not roman_string or not type(str):
+    if not roman_string:
+        return 0
+    elif not isinstance(roman_string, str):
         return 0
     elif roman_string in romans:
         return romans.get(roman_string)
