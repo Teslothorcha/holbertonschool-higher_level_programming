1a918af9d02c978831901e60d1226698e2bc8ee8
teslothorcha <juandavidmarinbernal@gmail.com>
2019-09-20 03:35:32 +0000

    [Fix] reduce 1 line

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    copy = list(map(lambda x: list(map(lambda y: y ** 2, x)), matrix))
-    return(copy)
+    return list(map(lambda x: list(map(lambda y: y ** 2, x)), matrix))
