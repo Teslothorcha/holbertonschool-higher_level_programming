228320965f4b804693fff9e5ea197bec84cbf81c
Teslothorcha <juandavidmarinbernal@gmail.com>
2019-09-30 19:24:02 +0000

    [Fix] add variable to count printing

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
+    i = 0
     try:
         for count in range(0, x):
             print(my_list[count], end="")
+            i += 1
         print()
-        return(count + 1)
+        return(i)
     except IndexError:
         print()
-        return(count)
+        return(i)
