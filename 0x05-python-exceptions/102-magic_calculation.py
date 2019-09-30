6726d0fc3f3bbe5b3a23519a109048aadd381d09
Teslothorcha <juandavidmarinbernal@gmail.com>
2019-09-30 23:06:28 +0000

    [Feature] add magic code

---
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def magic_calculation(a, b):
+    result = 0
+    for i in range(1, 3):
+        try:
+            if 1 > a:
+                raise Exception('Too far')
+            else:
+                result += (a ** b) / i
+        except:
+            result += a+b
+            break
+    return result
