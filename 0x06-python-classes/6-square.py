9f0c7c3a7726bab015ab3b2dfba1ecf8c20ad55f
Teslothorcha <juandavidmarinbernal@gmail.com>
2019-10-02 01:37:03 +0000

    [Feature] add getter and setter for position attribute

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+
+
+class Square:
+
+    def __init__(self, size=0, position=(0, 0)):
+        self.__size = size
+        self.__position = position
+
+    def area(self):
+        return (self.__size * self.__size)
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        if not isinstance(value, tuple):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if len(value) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if not isinstance(value[0], int) or not isinstance(value[1], int):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if value[0] < 0 or value[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
+    def my_print(self):
+        p = '#'
+        c = self.size
+        if self.size == 0:
+            print()
+        else:
+            for i in range(self.position[1]):
+                print()
+            while(c):
+                for j in range(0, self.position[0]):
+                    print(" ", end="")
+                print("{}".format(p) * self.size)
+                c -= 1
