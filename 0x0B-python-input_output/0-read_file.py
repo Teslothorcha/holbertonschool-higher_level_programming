efe44559d8ee88c97aab49e8490f9a044a78b3e0
teslothorcha <juandavidmarinbernal@gmail.com>
2019-10-16 21:58:47 +0000

    [Fix] read witouth loop

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def read_file(filename=""):
-    with open(filename, encoding"utf-8") as file:
-        for line in file:
-            print(file.read(), end="")
+    with open(filename, encoding="utf-8") as file:
+        print(file.read(), end="")
