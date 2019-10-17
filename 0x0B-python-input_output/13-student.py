312de49f12c6dd040580f3eda66ee407ea070221
teslothorcha <juandavidmarinbernal@gmail.com>
2019-10-17 01:36:40 +0000

    [Feature] updtae keys with dic

---
+++ 0x0B-python-input_output/13-student.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+class Student():
+        def __init__(self, first_name, last_name, age):
+                self.first_name = first_name
+                self.last_name = last_name
+                self.age = age
+
+        def to_json(self, attrs=None):
+                d = {}
+                if type(attrs) is list:
+                        s = attrs[:]
+                        for c in range(len(attrs)):
+                                if attrs[c] in self.__dict__:
+                                        d.setdefault(s[c], self.__dict__[s[c]])
+                        return d
+                else:
+                        return self.__dict__
+
+        def reload_from_json(self, json):
+                self.first_name = json['first_name']
+                self.last_name = json['last_name']
+                self.age = json['age']
