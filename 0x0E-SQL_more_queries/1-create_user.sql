e3e2a36d6f0a1b81756d098a13aac373b28a9cce
Teslothorcha <juandavidmarinbernal@gmail.com>
2019-11-26 20:00:57 -0500

    [Fix] crate user and give privileges IF NOT EXISTS

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
 -- create user with all priviliges
-CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'password';
+CREATE USER IF NOT EXISTS'user_0d_1'@'localhost' IDENTIFIED BY 'password';
 GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost';
