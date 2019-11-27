7e5e0b9ffe0f812e124787ad4780507e06ca3533
Teslothorcha <juandavidmarinbernal@gmail.com>
2019-11-26 23:47:45 -0500

    [Fix] cities tables

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -2,6 +2,6 @@
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities
        (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY  UNIQUE ,
-       	name varchar(256) NOT NULL,
-	state_id INT NOT NULL FOREIGN KEY(state_id)
-	REFERENCES FROM hbtn_0d_usa.cities(id));
+	state_id INT NOT NULL,
+	FOREIGN KEY(state_id) REFERENCES FROM hbtn_0d_usa.cities(id),
+	name varchar(256) NOT NULL);
