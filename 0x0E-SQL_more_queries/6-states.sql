bb056437325c6b1665079867f91c213f78c25b5b
Teslothorcha <juandavidmarinbernal@gmail.com>
2019-11-26 23:26:27 -0500

    [Fix] missing hierarchy

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,5 +1,5 @@
 -- createS table with two pks
 CREATE DATABASE hbtn_0d_usa IF NOT EXISTS;
-CREATE TABLE IF NOT EXISTS states
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states
        (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY  UNIQUE ,
        	name varchar(256) NOT NULL);
