966e931248de04d5fd5af85df8f0884763b5c42b
Teslothorcha <juandavidmarinbernal@gmail.com>
2019-12-15 19:44:56 -0500

    [Feature] query by argument given

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+import MySQLdb
+import sys
+
+
+def connect_database_2(usr, paswd, dt_name, st_name):
+    """ connects with
+    database to
+    gather information"""
+    db = MySQLdb.connect(host='localhost', user=usr, passwd=paswd,
+                         db=dt_name, port=3306)
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name = '{}' ORDER BY id ASC"
+                .format(st_name))
+    res = cur.fetchall()
+    for state in res:
+        print(state)
+    cur.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    """ Exectues if
+    main is calling it"""
+    try:
+        if sys.argv[4]:
+            connect_database_2(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
+    except:
+        pass
