7d3a5e1cbd1b6a42d239e74c8d7a33e1b2a563ee
Teslothorcha <juandavidmarinbernal@gmail.com>
2019-12-15 20:26:03 -0500

    [Feature] cities by given state

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+import MySQLdb
+import sys
+
+
+def connect_database_5(usr, paswd, dt_name, st_name):
+    """ connects with
+    database to
+    gather information"""
+    times = 1
+    db = MySQLdb.connect(host='localhost', user=usr, passwd=paswd,
+                         db=dt_name, port=3306)
+    cur = db.cursor()
+    cur.execute("SELECT c.name FROM cities c\
+    JOIN states s ON s.id = c.state_id WHERE s.name = '{}'\
+    ORDER BY c.id ASC".format(st_name))
+    res = cur.fetchall()
+    print(res)
+    for state in res:
+        if times < len(res):
+            print("{}, ".format(state[0]), end="")
+        else:
+            print("{}".format(state[0]))
+        times += 1
+    cur.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    """ Exectues if
+    main is calling it"""
+    try:
+        if sys.argv[4]:
+            connect_database_5(sys.argv[1], sys.argv[2], sys.argv[3],
+                               sys.argv[4])
+    except:
+        pass
