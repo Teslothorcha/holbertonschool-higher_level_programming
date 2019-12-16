415fb9c297ea4bba4537ebb92d3f876e346a0402
Teslothorcha <juandavidmarinbernal@gmail.com>
2019-12-15 19:04:48 -0500

    [Feature] first mysqldb

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+import MySQLdb
+import sys
+
+
+def getAllStates(usr, paswd, dt_name):
+    """ connects with
+    database to
+    gather information"""
+    db = MySQLdb.connect(host='localhost', user=usr, passwd=paswd,
+                         db=dt_name, port=3306)
+    cur = db.cursor()
+    cur.execute('SELECT * FROM states')
+    res = cur.fetchall()
+    for state in res:
+        print(state)
+    cur.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    """ Exectues if
+    main is calling it"""
+    try:
+        if sys.argv[3]:
+            getAllStates(sys.argv[1], sys.argv[2], sys.argv[3])
+    except:
+        pass
