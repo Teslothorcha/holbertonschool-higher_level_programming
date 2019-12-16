88281787b5ed0026a709d359e01dc7ce424aade9
Teslothorcha <juandavidmarinbernal@gmail.com>
2019-12-15 19:24:25 -0500

    [Feature] second mysqldb

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -3,14 +3,15 @@ import MySQLdb
 import sys
 
 
-def connect_database(usr, paswd, dt_name):
+def connect_database_1(usr, paswd, dt_name):
     """ connects with
     database to
     gather information"""
     db = MySQLdb.connect(host='localhost', user=usr, passwd=paswd,
                          db=dt_name, port=3306)
     cur = db.cursor()
-    cur.execute('SELECT * FROM states')
+    cur.execute('SELECT * FROM states WHERE name LIKE "N%"\
+    ORDER BY id ASC')
     res = cur.fetchall()
     for state in res:
         print(state)
@@ -23,6 +24,6 @@ if __name__ == "__main__":
     main is calling it"""
     try:
         if sys.argv[3]:
-            connect_database(sys.argv[1], sys.argv[2], sys.argv[3])
+            connect_database_1(sys.argv[1], sys.argv[2], sys.argv[3])
     except:
         pass
