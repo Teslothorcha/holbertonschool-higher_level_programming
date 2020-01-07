3aa353c1cfe1ff36478d0d948e22aaa287c87aef
Teslothorcha <juandavidmarinbernal@gmail.com>
2020-01-06 20:47:13 -0500

    [Feature] POST arg

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # size
-curl -sd "email=hr@holbertonschool.com, subject=I will always be here for PLD" "$1"
+curl -sd "email=hr@holbertonschool.com" -d "subject=I will always be here for PLD" "$1"
