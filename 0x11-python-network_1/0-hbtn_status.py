15b641770e5f918622a67985d326ad3a8d1c0fd2
Teslothorcha <juandavidmarinbernal@gmail.com>
2020-01-13 14:59:16 -0500

    [Fix] pep

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,8 +7,8 @@ if __name__ == "__main__":
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         html = response.read()
 
-        m2="- type: {t}".format(t=type(html))
-        m3="- content: {c}".format(c=html)
-        m4="- utf8 content: {d}".format(d=html.decode('utf-8'))
+        m2 = "- type: {t}".format(t=type(html))
+        m3 = "- content: {c}".format(c=html)
+        m4 = "- utf8 content: {d}".format(d=html.decode('utf-8'))
         print("Body response:")
         print("\t{}\n\t{}\n\t{}".format(m2, m3, m4))
