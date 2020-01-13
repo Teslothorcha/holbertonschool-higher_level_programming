439a738e8428a0ca6b571070216eab7513e99340
Teslothorcha <juandavidmarinbernal@gmail.com>
2020-01-13 14:42:37 -0500

    [Fix] cut lines

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,5 +7,6 @@ if __name__ == "__main__":
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         html = response.read()
 
-        print("\t- type: {t}\n\t- content: {c}\n\t- utf8 content:{d}".format(
-            t=type(html), c=html, d=html.decode('utf-8')))
+        print("Body response:\n\t- type: {t}\n\t- content: {c}\
+\n\t- utf8 content:{d}".format(
+    t=type(html), c=html, d=html.decode('utf-8')))
