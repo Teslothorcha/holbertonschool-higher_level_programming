76bbe16bab4a180f65ba7607588c7e5500ba281f
Teslothorcha <juandavidmarinbernal@gmail.com>
2020-01-13 17:26:49 -0500

    [Feature] error on rqeuest method

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -3,8 +3,9 @@
 import requests
 from sys import argv
 
+
 if __name__ == "__main__":
-    r = requests.post(argv[1])
+    r = requests.get(argv[1])
     if r.status_code == requests.codes.ok:
         print(r.text)
     else:
