a27d7d0294b61d77dc96adc6a1d138fad3d02dda
Teslothorcha <juandavidmarinbernal@gmail.com>
2020-02-04 20:39:54 -0500

    [Feature] factorial

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+let i = parseInt(process.argv.slice(2)[0]);
+if (isNaN(i)) {
+  i = 1;
+}
+const factorial = factfun(i);
+function factfun (n) {
+  if (n < 0) return;
+  if (n === 0) return 1;
+  return n * factfun(n - 1);
+}
+console.log(factorial);
