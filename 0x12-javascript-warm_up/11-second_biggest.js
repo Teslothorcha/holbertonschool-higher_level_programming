6fcc3a4c1dce60e5b29432cedaf8a53bbf6e4434
Teslothorcha <juandavidmarinbernal@gmail.com>
2020-02-04 21:18:39 -0500

    [Feature] second biggest

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -5,5 +5,7 @@ if (process.argv.slice(2).length === 0) {
 } else if (process.argv.slice(2).length === 1) {
   console.log('1');
 }
-const s = i.sort().length;
-console.log(i[s - 2]);
+if (process.argv.slice(2).length > 1) {
+  const s = i.sort().length;
+  console.log(i[s - 2]);
+}
