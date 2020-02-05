6aafcad2bc8bf6d0239695e059829384e8e36dcc
Teslothorcha <juandavidmarinbernal@gmail.com>
2020-02-04 20:05:58 -0500

    [Feature] loop of squares

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+let msg = '';
+if (parseInt(process.argv.slice(2)[0])) {
+  if (parseInt(process.argv.slice(2)[0]) >= 1) {
+    let i = parseInt(process.argv.slice(2)[0]);
+    let j = i;
+    while (i) {
+      msg += 'x';
+      i -= 1;
+    }
+    while (j) {
+      console.log(msg);
+      j -= 1;
+    }
+  }
+} else {
+  console.log('Missing size');
+}
