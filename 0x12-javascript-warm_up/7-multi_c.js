ef046ada764e28e2932c31c39c8622dbf93e1fc6
Teslothorcha <juandavidmarinbernal@gmail.com>
2020-02-04 19:53:37 -0500

    [Feature] loop of lang

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,10 +1,12 @@
 #!/usr/bin/node
 if (parseInt(process.argv.slice(2)[0])) {
-  const myVar = 'C is fun';
-  let i = parseInt(process.argv.slice(2)[0]);
-  while (i) {
-    console.log(myVar);
-    i -= 1;
+  if (parseInt(process.argv.slice(2)[0]) >= 1) {
+    const myVar = 'C is fun';
+    let i = parseInt(process.argv.slice(2)[0]);
+    while (i) {
+      console.log(myVar);
+      i -= 1;
+    }
   }
 } else {
   console.log('Missing number of occurrences');
