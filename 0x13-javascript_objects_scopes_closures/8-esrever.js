b0e0132f6afc92f8b535799d3ce21f61d6a9828f
Teslothorcha <juandavidmarinbernal@gmail.com>
2020-02-10 14:59:52 -0500

    [Fix] const instead of letr

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
   const len = list.length;
-  let newList = [];
+  const newList = [];
   for (let i = len - 1; i >= 0; i--) {
     newList.push(list[i]);
   }
