34bba9127dc5a8e3b7df3c03a72662bb4634836c
Teslothorcha <juandavidmarinbernal@gmail.com>
2020-02-11 13:29:48 -0500

    [Feature] film name

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const url = 'http://swapi.co/api/films/' + process.argv[2];
+const request = require('request');
+request.get(url, function (error, response, body) {
+  if (error) {
+    return console.log(error.toString());
+  }
+  console.log(JSON.parse(body).title);
+});
